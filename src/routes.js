84e70e6237fe9833d962891981e6e371700775b6
EL\ELISSA <harefaelisa11@gmail.com>
2023-03-23 16:04:09 +0700

    initial commit

---
+++ src/routes.js
@@ -0,0 +1,20 @@
+const routes = [
+  {
+    method: 'POST',
+    path: '/notes',
+    handler: addNoteHandler
+  },
+  {
+    method: 'GET',
+    path: '/notes',
+    handler: getAllNotesHandler
+  },
+  {
+    method: 'GET',
+    path: '/notes/{id}',
+    handler: getNoteByIdHandler
+  }
+]
+
+const { addNoteHandler, getAllNotesHandler, getNoteByIdHandler } = require('./handler')
+module.exports = routes
